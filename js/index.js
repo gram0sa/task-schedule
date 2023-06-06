21325725adce4e3e6783155b02455a49df8a4772
Lucas Gramosa <lucashalliier@gmail.com>
2023-06-06 00:52:21 -0300

    created: saving logic with JSON

--- js/index.js
+++ js/index.js
@@ -16,11 +16,11 @@ inputTarefa.addEventListener("keypress", (e) => {
   }
 });
 
-//captura o evento de click do botao 
+//captura o evento de click do botao
 btnTarefa.addEventListener("click", (e) => {
-    if (!inputTarefa.value) return console.log("Digite Algo");
-    criaTarefa(inputTarefa.value);
-  });
+  if (!inputTarefa.value) return console.log("Digite Algo");
+  criaTarefa(inputTarefa.value);
+});
 
 //adiciona o inputTarefa a <li>
 function criaTarefa(textoInput) {
@@ -34,28 +34,50 @@ function criaTarefa(textoInput) {
 
 //funcao para limpar o input
 function limpaInput() {
-    inputTarefa.value = "";
-    inputTarefa.focus();
+  inputTarefa.value = "";
+  inputTarefa.focus();
 }
 
-
+//funcao de criacao para de butao de apagar
 function btnApagaTarefa(li) {
-    li.innerHTML += ' ';
-    const btnApagar = document.createElement("button");
-    btnApagar.innerHTML = "Apagar";
-    btnApagar.setAttribute('class', 'apagar')
-    li.appendChild(btnApagar);
+  li.innerHTML += " ";
+  const btnApagar = document.createElement("button");
+  btnApagar.innerHTML = "Apagar";
+  btnApagar.setAttribute("class", "apagar");
+  li.appendChild(btnApagar);
 }
 
+//captura o evento de click do botao para apagar a <li>
 document.addEventListener("click", (e) => {
   const el = e.target;
 
-  if(el.classList.contains('apagar')) {
+  if (el.classList.contains("apagar")) {
     el.parentElement.remove();
   }
+  salvarTarefa();
 });
 
+//funcao para salvar a tarefa com JSON
 function salvarTarefa() {
-  const liTarefas = tarefa.querySelectorAll('li');
-  console.log(liTarefas); 
+  const liTarefas = tarefa.querySelectorAll("li");
+  const listaDeTarefas = [];
+
+  for (let tarefa of liTarefas) {
+    let tarefaTexto = tarefa.innerText;
+    tarefaTexto = tarefaTexto.replace("Apagar", " ").trim();
+    listaDeTarefas.push(tarefaTexto);
+  }
+
+  const tarefasJSON = JSON.stringify(listaDeTarefas);
+  localStorage.setItem("tarefas", tarefasJSON);
 }
+
+function addTarefasSalvas() {
+  const tarefas = localStorage.getItem("tarefas");
+  const listaDeTarefas = JSON.parse(tarefas);
+
+  for (let tarefa of listaDeTarefas) {
+    criaTarefa(tarefa);
+  }
+}
+addTarefasSalvas();
